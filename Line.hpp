a00b21fd92984cfb3eb8af75ba6b421a3dd24ee0
sergant446 <tsvu2009@mail.ru>
2024-03-16 14:28:40 +0300

    Created Line.hpp
    SFML Line source

---
+++ Line.hpp
@@ -0,0 +1,35 @@
+#ifndef LINE_HPP
+#define LINE_HPP
+#include <SFML/Graphics.hpp>
+class Line
+{
+public:
+	sf::Vertex points[2];
+	sf::Color color;
+
+	Line() : color(sf::Color::White) {}
+
+	Line(sf::Vector2f start, sf::Vector2f end) {
+		points[0] = sf::Vertex(start);
+		points[1] = sf::Vertex(end);
+	}
+
+	void setStart(sf::Vector2f start) {
+		points[0] = sf::Vertex(start);
+	}
+
+	void setEnd(sf::Vector2f end) {
+		points[1] = sf::Vertex(end);
+	}
+
+	void setColor(sf::Color newColor) {
+		color = newColor;
+		points[0].color = color;
+		points[1].color = color;
+	}
+
+	void draw(sf::RenderWindow& window) {
+		window.draw(points, 2, sf::Lines);
+	}
+};
+#endif // end of #ifndef LINE_HPP
